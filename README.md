a80010547440e925075ab2710890d89ab19e1214
Jhonatan Huanca <jh.anthony17@gmail.com>
2022-02-18 00:00:06 -0500

    Files finished

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# split-csv
+#split-csv
 This script helps you to split your csv files
+
+Was proved on Windows 10
+
+## Prerequesites
+`You'll need to install the follow packeage with pip:
+os
+argparse
+csv`
+
+## Usage
+`python split_csv.py <name of file to split> <lines you want in each output file>`
+
+You don't need to specify the extension csv.
+
+### Example
+`python split_csv.py file_to_split 1000`
+
+That means that the script will split the file 'file_to_split.csv' and it will put 1000 lines in each ouput file.
---
+++ split_csv.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Necesary Arguments:',epilog= "This an example to use it: py scrip_split_csv.py 'file_to_split.csv' 1000")
+
+parser.add_argument('name_csv', metavar = '[-Name]', type=str, help='Name of the csv to split')
+parser.add_argument('lines', metavar = '[-Lines]', type=int, help='Number of lines you want in each output file')
+args = parser.parse_args()
+file = args.name_csv
+file2 = 'vulns'
+
+def split(filehandler, delimiter=',', row_limit=args.lines, output_name_template='output_%s.csv', output_path='.', keep_headers=True):
+    
+    import csv
+    reader = csv.reader(filehandler, delimiter=delimiter)
+    current_piece = 1
+    current_out_path = os.path.join(
+         output_path,
+         output_name_template  % current_piece
+    )
+
+    class MyDialect(csv.Dialect):
+        delimiter = ','
+        quotechar = '"'
+        doublequote = True
+        skipinitialspace = False
+        lineterminator = '\n'
+        quoting = csv.QUOTE_MINIMAL
+
+    current_out_writer = csv.writer(open(current_out_path, 'w'), MyDialect())
+    current_limit = row_limit
+    if keep_headers:
+        headers = next(reader)
+        current_out_writer.writerow(headers)
+    for i, row in enumerate(reader):
+        if i + 1 > current_limit:
+            current_piece += 1
+            current_limit = row_limit * current_piece
+            current_out_path = os.path.join(
+               output_path,
+               output_name_template  % current_piece
+            )
+            current_out_writer = csv.writer(open(current_out_path, 'w'), MyDialect())
+            if keep_headers:
+                current_out_writer.writerow(headers)
+        current_out_writer.writerow(row)
+
+split(open(f'{file}.csv', 'r'));
